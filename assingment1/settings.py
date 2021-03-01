78bf495145028c21d6737395c417fc32584265d1
kapil-raj <kay.kapilraj@gmail.com>
2021-03-01 20:09:23 +0530

    finalstep

--- assingment1/settings.py
+++ assingment1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "9om$&k$qt9f-%=cj%xg#x_mop8t+h#ckw^vz#fkxlivfblaw)8"
+# SECRET_KEY = "9om$&k$qt9f-%=cj%kxagp#ixl_rmaojp8t+h#ckw^vz#fkxlivfblaw)8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
